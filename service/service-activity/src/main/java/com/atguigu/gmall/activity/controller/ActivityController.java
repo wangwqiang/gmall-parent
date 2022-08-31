e91c536c51478c8942f1f182ea6200202f6c0470
wangwenqiang <wangwqiang5215@163.com>
2022-09-01 01:27:31 +0800

    后台Api(活动列表CRUD)

--- service/service-activity/src/main/java/com/atguigu/gmall/activity/controller/ActivityController.java
+++ service/service-activity/src/main/java/com/atguigu/gmall/activity/controller/ActivityController.java
@@ -1,14 +1,16 @@
 package com.atguigu.gmall.activity.controller;
 
 import com.atguigu.gmall.activity.service.ActivityInfoService;
+import com.atguigu.gmall.activity.service.ActivityRuleService;
 import com.atguigu.gmall.common.result.Result;
 import com.atguigu.gmall.model.activity.ActivityInfo;
+import com.atguigu.gmall.model.activity.ActivityRule;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author wangwqiang
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ActivityController {
     @Autowired
     ActivityInfoService activityInfoService;
+    @Autowired
+    ActivityRuleService activityRuleService;
 
     /**
      * 查询活动列表分页信息
@@ -28,10 +32,78 @@ public class ActivityController {
      * @return
      */
     @GetMapping("/activityInfo/{pageNum}/{pageSize}")
-    public Result activityInfo(@PathVariable("pageNum")Long pageNum,
-                               @PathVariable("pageSize")Long pageSize){
-        Page<ActivityInfo> page = new Page<>(pageNum,pageSize);
+    public Result activityInfo(@PathVariable("pageNum") Long pageNum,
+                               @PathVariable("pageSize") Long pageSize) {
+        Page<ActivityInfo> page = new Page<>(pageNum, pageSize);
         Page<ActivityInfo> activityInfoPage = activityInfoService.page(page);
         return Result.ok(activityInfoPage);
     }
+
+    /**
+     * 添加活动
+     * @param activityInfo
+     * @return
+     */
+    @PostMapping("/activityInfo/save")
+    public Result save(@RequestBody ActivityInfo activityInfo) {
+        activityInfo.setCreateTime(new Date());
+        activityInfoService.save(activityInfo);
+        return Result.ok();
+    }
+
+    /**
+     * 根据id查询活动信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/activityInfo/get/{id}")
+    public Result getActivityInfo(@PathVariable("id") Long id) {
+        ActivityInfo activityInfo = activityInfoService.getById(id);
+        return Result.ok(activityInfo);
+    }
+
+    /**
+     * 根据id修改活动信息
+     * @param activityInfo
+     * @return
+     */
+    @PutMapping("/activityInfo/update")
+    public Result update(@RequestBody ActivityInfo activityInfo) {
+        activityInfoService.updateById(activityInfo);
+        return Result.ok();
+    }
+
+    /**
+     * 根据id删除活动信息
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/activityInfo/remove/{id}")
+    public Result remove(@PathVariable("id")Long id){
+        activityInfoService.removeById(id);
+        return Result.ok();
+    }
+
+    /**
+     * 批量删除活动信息
+     * @param ids
+     * @return
+     */
+    @DeleteMapping("/activityInfo/batchRemove")
+    public Result batchRemove(@RequestBody List<Long> ids){
+        activityInfoService.removeByIds(ids);
+        return Result.ok();
+    }
+
+    /**
+     * 根据活动id查询活动规则
+     * @param activityId
+     * @return
+     */
+    @GetMapping("/activityInfo/findActivityRuleList/{activityId}")
+    public Result findActivityRuleList(@PathVariable("activityId")Long activityId){
+        ActivityRule activityRule = activityRuleService.getById(activityId);
+        return Result.ok(activityRule);
+    }
+
 }
